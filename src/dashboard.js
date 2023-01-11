fe4e411a1b7a743187eee63dee1297d4e775b1d5
Shehzad <shehzaddiqbal@gmail.com>
2023-01-11 14:39:17 +0500

    sending data

--- src/dashboard.js
+++ src/dashboard.js
@@ -4,7 +4,6 @@ import { db } from "./firebase.js";
 import {
   signOut,
   onAuthStateChanged,
-  getAuth,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js"; //CDN
 import {
   getFirestore,
@@ -23,57 +22,78 @@ import {
   limit,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js"; //CDN
 
-const savePost = async () => {
+// const savePost =
+(async () => {
   try {
     await onAuthStateChanged(auth, async (user) => {
       // to find the ID of Logged in user
       // console.log(user);
       await setDoc(doc(db, user.uid, "cash"), {
         amount: 0,
+        category: "default",
         createdOn: serverTimestamp(),
       });
 
       await setDoc(doc(db, user.uid, "savings"), {
         amount: 0,
+        category: "default2",
         createdOn: serverTimestamp(),
       });
 
       await setDoc(doc(db, user.uid, "transactionsHistory"), {
         amount: 0,
+        category: "default",
         createdOn: serverTimestamp(),
       });
-
-
-
-
-
-
 
       // await addDoc(collection(db, user.uid), {dataObj });
     });
   } catch (e) {
     console.error("Error adding document: ", e);
   }
+})();
+// savePost();
+
+const addAmountFun = () => {};
+
+const transactionFun = async () => {
+  const inputValuee = Number(document.querySelector("#inputAmount").value);
+
+  const selectedAccount = document.querySelector("#typeSelect").value;
+  if (selectedAccount === "none") return alert("please select another account");
+  try {
+    await onAuthStateChanged(auth, async (user) => {
+      await updateDoc(doc(db, user.uid, selectedAccount), {
+        amount: inputValuee,
+      });
+    });
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+
+  // data.totalAmount =
+
+  // rerendering();
+  // console.log(data); ///////
 };
-savePost();
+// transactionFun()
+// document.querySelector("#expenseBtn").addEventListener("click", () => {
+//   data.totalAmount -= Number(document.querySelector("#inputAmount").value);
+//   document.querySelector("#amountValue").innerHTML = data.totalAmount;
 
+//   localStorage.setItem("expenseData", JSON.stringify(data)); ///
+//   rerendering(); /////
+// });
 
+document.querySelector("#incomeBtn").addEventListener("click", transactionFun);
 
-
-
-
-
-
-
-
-// let unsubscribe;
 // (() => {
 //   const q = query(
 //     collection(db, "posts"),
 //     orderBy("createdOn", "desc"),
 //     limit(60)
 //   );
-//   unsubscribe = onSnapshot(q, (querySnapshot) => {
+//     onSnapshot(q, (querySnapshot) => {
 //     const posts = [];
 //     querySnapshot.forEach((doc) => {
 //       posts.push({ id: doc.id, ...doc.data() });
@@ -83,11 +103,6 @@ savePost();
 //     console.log("posts", posts);
 //   });
 // })();
-
-// //this is useEffect cleanup and is called when i leave this useEffect
-// return () => {
-//   unsubscribe();
-// };
 
 // //
 // try {
@@ -100,21 +115,6 @@ savePost();
 //   console.error("Error adding document: ", e);
 // }
 // //
-
-// signoutBtn.addEventListener("click", () => {
-//   signOut(auth)
-//     .then(() => {
-//       window.location.href = "index.html";
-//       console.log("logout");
-//     })
-//     .catch((error) => {
-//       // An error happened.
-//       console.log("login");
-//     });
-// });
-
-// const deletePost = async () => await deleteDoc(doc(db, "posts", props.id));
-// // console.log("postId: ", props.id);
 
 // const updatePost = async (e) => {
 //   setDropDown(false);
@@ -129,64 +129,56 @@ savePost();
 //   setDropDown(false);
 // };
 
-// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+// $$$$$$$$$$$$$$$$$$$$$$$$$- Local Storage Approach -$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
-let data = JSON.parse(localStorage.getItem("expenseData")) || {
-  userName: "Shehzad",
-  totalAmount: 0,
-  accounts: { cash: 0, bank: 0, saving: 0 },
-};
-//
-// new logic
-// arrayOfTodos.push(todo);
-// localStorage.setItem("expenseData", JSON.stringify(data));
+// let data = JSON.parse(localStorage.getItem("expenseData")) || {
+//   userName: "Shehzad",
+//   totalAmount: 0,
+//   accounts: { cash: 0, bank: 0, saving: 0 },
+// };
 
-// document.querySelector("#todoList").innerHTML = ``;
-// document.querySelector("#todoItem").value = ``;
+// const transaction = () => {
+//   const inputValuee = Number(document.querySelector("#inputAmount").value);
 
-// let data.totalAmount = 0;
-// document.querySelector("#amountValue").innerHTML = data.totalAmount; ///////
+//   const select = document.querySelector("#typeSelect").value;
+//   if (select === "cash") {
+//     data.accounts.cash += inputValuee;
+//   } else if (select === "saving") {
+//     data.accounts.saving += inputValuee;
+//   } else if (select === "bank") {
+//     data.accounts.bank += inputValuee;
+//   } else {
+//     alert("please select another account");
+//   }
+//   data.totalAmount =
+//     data.accounts.cash + data.accounts.saving + data.accounts.bank;
+//   localStorage.setItem("expenseData", JSON.stringify(data)); ///
+//   rerendering();
+//   console.log(data); ///////
+// };
 
-const transaction = () => {
-  const inputValuee = Number(document.querySelector("#inputAmount").value);
+// document.querySelector("#expenseBtn").addEventListener("click", () => {
+//   data.totalAmount -= Number(document.querySelector("#inputAmount").value);
+//   document.querySelector("#amountValue").innerHTML = data.totalAmount;
 
-  const select = document.querySelector("#typeSelect").value;
-  if (select === "cash") {
-    data.accounts.cash += inputValuee;
-  } else if (select === "saving") {
-    data.accounts.saving += inputValuee;
-  } else if (select === "bank") {
-    data.accounts.bank += inputValuee;
-  } else {
-    alert("please select another account");
-  }
-  data.totalAmount =
-    data.accounts.cash + data.accounts.saving + data.accounts.bank;
-  localStorage.setItem("expenseData", JSON.stringify(data)); ///
-  rerendering();
-  console.log(data); ///////
-};
+//   localStorage.setItem("expenseData", JSON.stringify(data)); ///
+//   rerendering(); /////
+// });
 
-document.querySelector("#expenseBtn").addEventListener("click", () => {
-  data.totalAmount -= Number(document.querySelector("#inputAmount").value);
-  document.querySelector("#amountValue").innerHTML = data.totalAmount;
+// document.querySelector("#incomeBtn").addEventListener("click", transaction);
 
-  localStorage.setItem("expenseData", JSON.stringify(data)); ///
-  rerendering(); /////
-});
-
-document.querySelector("#incomeBtn").addEventListener("click", transaction);
+// $$$$$$$$$$$$$$$$$$$$$$$$$- Local Storage Approach -$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 //re-rendering to update data
 const rerendering = () => {
-  document.querySelector("#cashAmountSpan").innerHTML = data.accounts.cash;
-  document.querySelector("#savingAmountSpan").innerHTML = data.accounts.saving;
-  document.querySelector("#bankAmountSpan").innerHTML = data.accounts.bank;
-
-  document.querySelector("#amountValue").innerHTML = data.totalAmount;
+  // document.querySelector("#cashAmountSpan").innerHTML = data.accounts.cash;
+  // document.querySelector("#savingAmountSpan").innerHTML = data.accounts.saving;
+  // document.querySelector("#bankAmountSpan").innerHTML = data.accounts.bank;
+  // document.querySelector("#amountValue").innerHTML = data.totalAmount;
 };
 rerendering();
 
+//LOG-OUT System
 document.querySelector("#logOutBtn").addEventListener("click", async () => {
   const res = confirm("Are you sure you wanna logOut");
   if (res) {
@@ -207,8 +199,7 @@ document.querySelector("#logOutBtn").addEventListener("click", async () => {
 const userAuthState = async () => {
   await onAuthStateChanged(auth, (user) => {
     if (user) {
-      // console.log(user);
-      // console.log(user.uid);
+      // console.log(user);user.uid
     } else {
       window.location.href = "./login.html";
       console.log("User is signed out");
